95e515e33d5fa6692f9aa5e5809074888ec01feb
raihanuldev <rihanthedev@gmail.com>
2024-12-25 23:29:16 +0600

    day two

---
+++ codeChef/2ndDay.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    
+    // cout << "1 2 3 4 5";
+    // cout << 8+4<<endl;
+    // cout << 8-4<<endl;
+    // cout << 8/4<<endl;
+    // cout << 8*4<<endl;
+    return 0;
+}
