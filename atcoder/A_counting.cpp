bfb1d0f3a51dd994514904103fe729623633e63d
raihanuldev <rihanthedev@gmail.com>
2025-03-25 00:45:07 +0600

    atcoder A_counting

---
+++ atcoder/A_counting.cpp
@@ -0,0 +1,19 @@
+// Submission Link: https://atcoder.jp/contests/abc209/submissions/64169164
+// Date: 25/03/2025
+//author: raihanuldev
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int count =0;
+    for(int i = a; i <=b;i++)
+    {
+        count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
