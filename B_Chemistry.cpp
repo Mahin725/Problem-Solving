5a347985385a170cfca3b0e72f5862229f2a6f3c
raihanuldev <rihanthedev@gmail.com>
2025-03-26 03:23:18 +0600

    add

---
+++ B_Chemistry.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tcase;cin>>tcase;
+    while(tcase--)
+    {
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        
+    }
+    return 0;
+}
