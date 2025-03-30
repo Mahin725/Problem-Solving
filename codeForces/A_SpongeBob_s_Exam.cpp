3cfaf0ae6bc1f98e7753126580ae894eff4967b2
raihanuldev <rihanthedev@gmail.com>
2025-03-30 10:14:32 +0600

    added

---
+++ codeForces/A_SpongeBob_s_Exam.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t>>n;
+    if(t>=n)
+    {
+        cout<<"Passed :)"<<endl;
+    }else{
+        cout<<"Failed :("<<endl;
+    }
+    return 0;
+}
