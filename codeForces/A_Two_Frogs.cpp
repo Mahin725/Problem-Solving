816c5e7c1c53d8a3642096d37db9569a82a3705f
raihanuldev <rihanthedev@gmail.com>
2025-04-06 15:53:13 +0600

    add

--- codeForces/A_Two_Frogs.cpp
+++ codeForces/A_Two_Frogs.cpp
@@ -1,28 +1,28 @@
-/**
- * Bismillahhir Rahmanir Rahim
- * author: Raihanul Islam Sharif
- * Problem link: https://codeforces.com/problemset/problem/2055/A
- * platform: div 2 codeforces
- * Date: 06 - 04 - 2025
-*/
+    /**
+     * Bismillahhir Rahmanir Rahim
+     * author: Raihanul Islam Sharif
+     * Problem link: https://codeforces.com/problemset/problem/2055/A
+     * platform: div 2 codeforces
+     * Date: 06 - 04 - 2025
+    */
 
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int tcase;cin>>tcase;
-    while(tcase--)
+    int main()
     {
-        int n,a,b;
-        cin>>n>>a>>b;
-        if(abs(a - b) % 2 == 1){
-            cout<<"NO"<<endl;
-        }else{
-            cout<<"YES"<<endl;
-        }
-    }   
-    return 0;
-}
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int tcase;cin>>tcase;
+        while(tcase--)
+        {
+            int n,a,b;
+            cin>>n>>a>>b;
+            if(abs(a - b) % 2 == 1){
+                cout<<"NO"<<endl;
+            }else{
+                cout<<"YES"<<endl;
+            }
+        }   
+        return 0;
+    }
