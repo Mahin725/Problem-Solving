825e045875484e1b47ae442bb5cc1224f6af5af7
raihanuldev <rihanthedev@gmail.com>
2025-04-07 21:59:51 +0600

    add

--- codeForces/B_Lady_Bug.cpp
+++ codeForces/B_Lady_Bug.cpp
@@ -1,40 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int tcase;
-    cin >> tcase;
-    while (tcase--)
-    {
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
         int n;
         cin >> n;
         string a, b;
         cin >> a >> b;
 
-        bool possible = true;
+        int a_odd_ones = 0, a_even_ones = 0;
+        int b_odd_zeros = 0, b_even_zeros = 0;
 
-        if(a[0] =='1' && b[0]!='1')
-        {
-            possible = false;
-        }
-        for (int i = 1; i < n; i++)
-        {
-            if (a[i] == '1' && b[i-1]!='0')
-            {
-                possible = false;
-                break;
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) { 
+                if (a[i] == '1') a_even_ones++;
+                if (b[i] == '0') b_even_zeros++;
+            } else {
+                if (a[i] == '1') a_odd_ones++;
+                if (b[i] == '0') b_odd_zeros++;
             }
-            
         }
-        cout << (possible ? "YES" : "NO") << endl;
-    }
 
+        if (a_even_ones <= b_odd_zeros && a_odd_ones <= b_even_zeros) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
     return 0;
 }
-
-/**
- * main logic i think problem.
- */
