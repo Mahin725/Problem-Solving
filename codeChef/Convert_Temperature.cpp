f88cd840edee0a21a02a559d3d48e6377790e3fa
raihanuldev <rihanthedev@gmail.com>
2025-04-10 02:11:51 +0600

    2

---
+++ codeChef/Convert_Temperature.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    double tmp = 25.5;
+    cout<<"Celsius - "<<tmp<<endl;
+    cout<<"Kelvin  - "<<tmp+273<<endl;
+}
