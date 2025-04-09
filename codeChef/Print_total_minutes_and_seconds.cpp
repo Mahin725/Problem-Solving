c709292834f197fd53975ca27a3fddb4a017b266
raihanuldev <rihanthedev@gmail.com>
2025-04-10 02:14:00 +0600

    3

---
+++ codeChef/Print_total_minutes_and_seconds.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+    int hour = 5;
+    int minit = hour*60;
+    cout<<minit<<endl;
+    cout<<minit*60<<endl;
+    
+
+}
