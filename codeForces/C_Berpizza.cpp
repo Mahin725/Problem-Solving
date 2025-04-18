1c93dabea69ae5d3fb7ac1cebf6a2f3c85710a87
raihanuldev <rihanthedev@gmail.com>
2025-04-18 10:21:51 +0600

    C. Berpizza

--- codeForces/C_Berpizza.cpp
+++ codeForces/C_Berpizza.cpp
@@ -6,36 +6,42 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n,customerNo=1;
-    cin>>n;
-    set<pair<int,int>> s;
-    multiset<pair<int,int>>ml;
-    vector<int>ans;
-    for(int i =1; i<=n;i++)
+    int n, customerNo = 1;
+    cin >> n;
+    set<pair<int, int>> s;
+    multiset<pair<int, int>> ml;
+    vector<int> ans;
+
+    for (int i = 1; i <= n; i++)
     {
         int type;
-        cin>>type;
-        if(type==1){
+        cin >> type;
+        if (type == 1) {
             int money;
-            cin>>money;
-            s.insert({customerNo,money});
-            ml.insert({money,-customerNo});
+            cin >> money;
+            s.insert({customerNo, money});
+            ml.insert({money, -customerNo});
             customerNo++;
-        }else if(type ==2){
-            int pos = s.begin()->first, money = s.begin()->second;
+        } 
+        else if (type == 2) {
+            int pos = s.begin()->first;
+            int money = s.begin()->second;
             ans.push_back(pos);
             s.erase(s.begin());
-            ml.erase({money,pos});
-        }else{
-            int pos=-ml.rbegin()->second,money = ml.rbegin()->first;
+            ml.erase(ml.find({money, -pos}));
+        } 
+        else { // type == 3
+            int money = ml.rbegin()->first;
+            int pos = -ml.rbegin()->second;
             ans.push_back(pos);
-            ml.erase(--ml.end());
-            s.erase({pos,money});
+            ml.erase(prev(ml.end()));
+            s.erase({pos, money});
         }
     }
-    for(auto val:ans)
-    {
-        cout<<val<<" ";
-    }
+
+    for (auto val : ans)
+        cout << val << " ";
+    cout << "\n";
+
     return 0;
 }
