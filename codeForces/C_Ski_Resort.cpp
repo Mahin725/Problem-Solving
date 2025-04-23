cb4c9fc1f9e4fdffc32544cdc36f2ca919f7e2c6
raihanuldev <rihanthedev@gmail.com>
2025-04-23 16:03:29 +0600

    Add solution for C. Ski Resort problem

---
+++ codeForces/C_Ski_Resort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcase;
+    cin >> tcase;
+
+    while (tcase--) {
+        int n, k, q;
+        cin >> n >> k >> q;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        ll ans = 0, cons = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] <= q) {
+                cons++;
+                
+                if (i == n - 1 || a[i + 1] > q) {
+                    if (cons >= k) {
+                        ll x = cons - k + 1;
+                        ll add = (x * (x + 1)) / 2;
+                        ans += add;
+                    }
+                    cons = 0; 
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
