48554c41abb8481959a3c4c1992ba08fc6f628c1
raihanuldev <rihanthedev@gmail.com>
2025-04-25 16:40:49 +0600

    A. Jellyfish and Undertale solution for improved readability and structure

--- codeForces/A_Jellyfish_and_Undertale.cpp
+++ codeForces/A_Jellyfish_and_Undertale.cpp
@@ -5,15 +5,28 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tcase;cin>>tcase;
-    while(tcase--)
+
+    int tcase;
+    cin >> tcase;
+    
+    while (tcase--)
     {
-        int a,b,n;
-        cin>>a>>b>>n;
-        vector<int>tools(n);
-        for(int i =0; i,n;i++)
-            cin>>tools[i];
-        
-    }   
+        int a, b, n;
+        cin >> a >> b >> n;
+
+        vector<int> tools(n);
+        for (int i = 0; i < n; i++)
+            cin >> tools[i];
+
+        long long total = b;
+
+        for (int i = 0; i < n; i++)
+        {
+            total += min((long long)tools[i] + 1, (long long)a) - 1;
+        }
+
+        cout << total << '\n';
+    }
+
     return 0;
 }
