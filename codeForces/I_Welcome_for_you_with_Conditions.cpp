f591be297ec0d080af18039256fa6449a51689b6
raihanuldev <rihanthedev@gmail.com>
2025-04-26 03:54:49 +0600

    Add initial implementation for I. Welcome for you with Conditions problem

---
+++ codeForces/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,22 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : I_Welcome_for_you_with_Conditions.cpp
+ * platform: 
+ * Date: 26 - 04 - 2025
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a,b;
+    cin>>a>>b;
+    if(a>=b)
+        cout<<"Yes";
+    else
+        cout<<"No";   
+    return 0;
+}
