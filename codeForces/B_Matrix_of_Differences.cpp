d6bda0b0e9085626ed70314a29c4fe5d3e76f14f
raihanuldev <rihanthedev@gmail.com>
2025-05-07 06:20:22 +0600

    Add implementation for B. Matrix of Differences problem

---
+++ codeForces/B_Matrix_of_Differences.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void raihanuldev()
+{
+    int n;
+    cin>>n;
+    int x=1,y = n*n;
+    vector<int>v;
+    for(int i=1; i<=n*n; i++)
+    {
+        if(i%2==1)
+        {
+            v.push_back(x);
+            x++;
+        }
+        else
+        {
+            v.push_back(y);
+            y--;
+        }
+    }
+    x=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(i%2==1)
+        {
+            for(int j=x; j<x+n; j++)cout<<v[j]<<" ";
+            cout<<endl;
+            x+=n;
+
+        }
+        else
+        {
+            for(int j=(n+x)-1; j>=x; j--)cout<<v[j]<<" ";
+            cout<<endl;
+            x+=n;
+        }
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tcase;cin>>tcase;
+    while(tcase--)
+    {
+        raihanuldev();
+    }   
+    return 0;
+}
