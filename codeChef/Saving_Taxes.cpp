0b70087e6cebe93a288e8130e0b2691de7fd40ce
raihanuldev <rihanthedev@gmail.com>
2025-05-16 04:22:42 +0600

    Add solution for CodeChef problem Saving Taxes

---
+++ codeChef/Saving_Taxes.cpp
@@ -0,0 +1,24 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : Saving_Taxes.cpp
+ * platform:
+ * Date: 16 - 05 - 2025
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x - y<<endl;
+    }
+    return 0;
+}
