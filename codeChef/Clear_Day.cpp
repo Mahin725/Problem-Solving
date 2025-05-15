4e9edc05ee5cdaf06e6695172e454ff968ae9cce
raihanuldev <rihanthedev@gmail.com>
2025-05-16 04:16:34 +0600

    Add solution for CodeChef problem Clear Day

---
+++ codeChef/Clear_Day.cpp
@@ -0,0 +1,20 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : Clear_Day.cpp
+ * platform:
+ * Date: 16 - 05 - 2025
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,y;
+    cin>>x>>y;
+    int remaining = 7-x-y;
+    cout<<remaining<<endl;
+    return 0;
+}
