3c6787db493b7307a0554e6814d577b3be51afa4
raihanuldev <rihanthedev@gmail.com>
2025-05-16 04:14:34 +0600

    Add solution for CodeChef problem Lucky Seven

---
+++ codeChef/Lucky_Seven.cpp
@@ -0,0 +1,19 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : Lucky_Seven.cpp
+ * platform: 
+ * Date: 16 - 05 - 2025
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin>>s;
+    cout<<s[6]<<endl;
+    return 0;
+}
