869356e64ed9d33b6d3e69bcccbc4d991b1047f0
raihanuldev <rihanthedev@gmail.com>
2025-05-16 04:18:14 +0600

    Add solution for CodeChef problem Double Rent

---
+++ codeChef/Double_Rent.cpp
@@ -0,0 +1,19 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : Double_Rent.cpp
+ * platform: 
+ * Date: 16 - 05 - 2025
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x;
+    cin>>x;
+    cout<<x*2<<endl;   
+    return 0;
+}
