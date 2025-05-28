7aeb4f05d158ee7ca1bd84e6814ecbdda63c3455
raihanuldev <rihanthedev@gmail.com>
2025-05-28 15:57:29 +0600

    Add solution for Codeforces problem A. Maximum GCD from Round 651 (Div. 2)

---
+++ codeForces/A_Maximum_GCD.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tcase;
+    cin>>tcase;
+    while(tcase--)
+    {
+        int n;
+        cin>>n;
+        int maxGCD= n/2;
+        cout<<maxGCD<<endl;
+    }   
+    return 0;
+}
