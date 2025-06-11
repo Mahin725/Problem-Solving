e8e1102dafd2e38a02fb7f367b72479fadbff301
raihanuldev <rihanthedev@gmail.com>
2025-06-12 04:50:04 +0600

    Add solution for LeetCode problem: 334. Increasing Triplet Subsequence

---
+++ LeetCode/leetcode 75/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,32 @@
+/**
+ * Bismillahhir Rahmanir Rahim
+ * author: Raihanul Islam Sharif
+ * Problem : 334. Increasing Triplet Subsequence.cpp
+ * platform: 
+ * Date: 12 - 06 - 2025
+ */
+
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size()<3) return false;
+        int left = INT_MAX;
+        int mid = INT_MAX;
+
+        for(int i=0; i<nums.size();i++)
+        {
+            if(nums[i] >mid){
+                return true;
+            }
+            else if(nums[i] > left && nums[i] <mid){
+                mid = nums[i];
+            }
+            else if(nums[i] < left)
+            {
+                left = nums[i];
+            }
+        }
+        return false;
+    }
+};
