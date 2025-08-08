c2e07e68e069cd5d968d32b906691044a686c008
raihanuldev <rihanthedev@gmail.com>
2025-08-08 23:38:07 +0600

    soultions added

---
+++ codeForces/A_Police_Recruits.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    int available = 0;   
+    int untreated = 0; 
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        if (x == -1) {
+            if (available > 0) {
+                --available;
+            } else {
+                ++untreated;
+            }
+        } else {
+            available += x;
+        }
+    }
+
+    cout << untreated << '\n';
+    return 0;
+}
