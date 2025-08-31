4f08333e272d65df7719effaf2ad1f58abaed407
raihanuldev <rihanthedev@gmail.com>
2025-09-01 03:50:42 +0600

    insertion_sort vs builtIn_sort()

--- worktech/array/insertion_sort.cpp
+++ worktech/array/insertion_sort.cpp
@@ -6,12 +6,27 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int arr[] = {4, 5, 1, 2, 7, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // cout << size;
+    // there are we will implemet insertion sort
 
-    vector<int> a = {4, 5, 1, 2, 7, 3};
-    sort(a.begin(), a.end());
-    for (auto value : a)
+    for (int i = 1; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    for (auto value : arr)
     {
         cout << value << ",";
     }
+
     return 0;
 }
