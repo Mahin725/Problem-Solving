4af3e08fe67a87b4283f29e05047e860758ea8b9
raihanuldev <rihanthedev@gmail.com>
2025-08-31 23:52:44 +0600

    A_word_capitallizations

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,17 @@
+// Author: Raihanul Islam
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string text;
+    cin >> text;
+
+    text[0] = toupper(text[0]);
+
+    cout << text << endl;
+    return 0;
+}
