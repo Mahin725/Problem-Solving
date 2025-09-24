05d823f9e569f1202f67a7411e3e14670ee2143d
raihanuldev <rihanthedev@gmail.com>
2025-09-24 12:31:09 +0600

    sorting

---
+++ codeForces/A_Equidistant_Letters.cpp
@@ -0,0 +1,25 @@
+// Author: Raihanul Islam
+#include <bits/stdc++.h>
+using namespace std;
+
+void raihanuldev()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcase;
+    cin >> tcase;
+    while (tcase--)
+    {
+        raihanuldev();
+    }
+    return 0;
+}
