981b1f9caed4a1628922d736371e3dea27620058
raihanuldev <rihanthedev@gmail.com>
2025-10-05 03:11:31 +0600

    added

---
+++ frit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // char s[50];
+    // scanf("%s", &s);
+    // printf("%s", s);
+    char s[50];
+    scanf("%s", &s); // Hello
+    printf("Hello, %s", s);
+}
