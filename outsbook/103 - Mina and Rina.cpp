7ab33745d97b73865034223e240ab37ed0616fe8
raihanuldev <rihanthedev@gmail.com>
2025-10-30 04:25:58 +0600

    103 - Mina and Rina

---
+++ outsbook/103 - Mina and Rina.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    int mina = (x/2)+(y/2);
+    int rina = (x/2)-(y/2);
+    cout<<mina <<" "<<rina<<endl;
+
+}
