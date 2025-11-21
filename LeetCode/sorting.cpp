bb3613bfba58a44964bc792c0f70e45b7c6ee5dd
raihanuldev <rihanthedev@gmail.com>
2025-11-22 01:22:07 +0600

    weekly contest

--- LeetCode/sorting.cpp
+++ LeetCode/sorting.cpp
@@ -1,14 +1,25 @@
 /**
  * "In the remembrance of Allah do hearts find rest." - Quran 13:28
- * 
- */ 
+ *
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int maximizeExpressionOfThree(vector<int> &nums)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    int c = nums[0];
+    int a = nums[n - 2];
+    int b = nums[n - 1];
+    cout<< a + b - c;
+    return 0;
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
-    
-    return 0;
+    vector<int>num={1,4,2,5};
+    maximizeExpressionOfThree(num);
 }
