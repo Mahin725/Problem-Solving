30afd5216a8dad296ddb2791c081ab45ebfda9e9
raihanuldev <rihanthedev@gmail.com>
2025-11-22 00:44:38 +0600

    added

---
+++ LeetCode/sorting.cpp
@@ -0,0 +1,14 @@
+/**
+ * "In the remembrance of Allah do hearts find rest." - Quran 13:28
+ * 
+ */ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    
+    return 0;
+}
