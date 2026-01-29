e85fff6e7cf6b3880b3cc3a6f56fd07e9b7099b9
mahin725 <rihanthedev@gmail.com>
2026-01-30 00:10:39 +0600

    added

---
+++ RandomThinking/googleDocCodePractice.cpp
@@ -0,0 +1,19 @@
+// Problem: take two input and check is greater or not.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	if(a>b) {
+		cout<<a<<" is greater then"<<b<<endl; 	
+	}else{
+
+		cout<<b<<"is better than"<<endl;
+	}
+	return 0;
+}
+
+
+// in this problem i face frist of all header files include type mistakes. then we i face an issues from google doc " quations marks" relted issues/ thanksss
