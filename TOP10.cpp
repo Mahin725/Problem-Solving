f8a70367fec4774f99ead685d0996ebc5859e62b
mahin725 <rihanthedev@gmail.com>
2026-02-07 13:15:47 +0600

    add Double_Rent and Saving_Taxes implementations

---
+++ TOP10.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tcase; cin>>tcase;
+	while(tcase--){
+	    int n; cin>>n;
+	    if(n<=10){
+	        cout<<"YES\n";
+	    }else{
+	        cout<<"NO\n";
+	    }
+	}
+
+}
