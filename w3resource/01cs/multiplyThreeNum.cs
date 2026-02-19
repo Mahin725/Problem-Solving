f0a8109ac2d5c04edc1fe278917c1a20cd187b94
mahin725 <rihanthedev@gmail.com>
2026-02-19 23:07:55 +0600

    aded

---
+++ w3resource/01cs/multiplyThreeNum.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program{
+    public static void Main(string[] arg){
+        int frist=Convert.ToInt32( Console.ReadLine());
+        int secound = Convert.ToInt32(Console.ReadLine());
+        int third = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(frist*secound*third);
+        
+    }
+}
