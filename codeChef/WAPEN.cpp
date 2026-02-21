98a4b47a706c169136bd5df64fdc9fa055c10921
mahin725 <rihanthedev@gmail.com>
2026-02-21 16:42:58 +0600

    WAPEN

---
+++ codeChef/WAPEN.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    int incorrectSubmission = y*10;
+    cout<<x+incorrectSubmission<<endl;
+}
