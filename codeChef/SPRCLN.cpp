3e38ba21f8c255d28ff332bbab05ef9c54dc2996
mahin725 <rihanthedev@gmail.com>
2026-02-24 08:51:27 +0600

    SPRCLN

---
+++ codeChef/SPRCLN.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+  int x,y;
+  cin>>x>>y;
+  long long x_total = x *30;
+  long long y_total = y *60;
+  cout<<(x_total+y_total)<<endl;
+}
