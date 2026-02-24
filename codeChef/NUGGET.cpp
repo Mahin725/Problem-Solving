5737f59daa5da858f3ecc463b01e2926be482a2a
mahin725 <rihanthedev@gmail.com>
2026-02-24 09:13:36 +0600

    NUGGET

---
+++ codeChef/NUGGET.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    int xT = x*5000;
+    int yt = y*9800;
+    cout<<(xT+yt)<<endl;
+}
