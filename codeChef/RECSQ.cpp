4be1202c5b042b6c41d20a47270e5b38d2bd0f2b
mahin725 <rihanthedev@gmail.com>
2026-02-25 07:54:38 +0600

    RECSQ

---
+++ codeChef/RECSQ.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+  int a,b,c;
+  cin>>a>>b>>c;
+  int x=a*b;
+	int y=c*c;
+	if(x==y)cout << "Yes\n";
+	else cout << "No\n";
+}
