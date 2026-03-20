9a0d656ecbfa19a418a6d5c991eaf4b5add22327
mahin725 <rihanthedev@gmail.com>
2026-03-20 06:12:16 +0600

    added Company info

--- LeetCode/stl/771. Jewels and Stones.cpp
+++ LeetCode/stl/771. Jewels and Stones.cpp
@@ -1,13 +1,20 @@
-class Solution {
+// Previously Asked In
+//     Apple 5 Meta 2 Uber 2 Adobe 2 Amazon 2 Google 2 Yandex 2 Microsoft 2
+
+class Solution
+{
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        map<char,int>mp;
-        for(auto ch:stones){
+    int numJewelsInStones(string jewels, string stones)
+    {
+        map<char, int> mp;
+        for (auto ch : stones)
+        {
             mp[ch]++;
         }
-        int count =0;
-        for(auto ch:jewels){
-            count+=mp[ch];
+        int count = 0;
+        for (auto ch : jewels)
+        {
+            count += mp[ch];
         }
         return count;
     }
