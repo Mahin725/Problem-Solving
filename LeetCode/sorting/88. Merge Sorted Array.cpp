6048b25fc555cfa0d6ceda2ea5c98dc0e40d74ae
mahin725 <rihanthedev@gmail.com>
2026-04-30 05:55:43 +0600

    add implementation for merging two sorted arrays

---
+++ LeetCode/sorting/88. Merge Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int cnt =0;
+        for(int i=m;i<nums1.size();i++){
+            nums1[i]=nums2[cnt];
+            cnt++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
+
+// o(m) + 
